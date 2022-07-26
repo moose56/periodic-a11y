147bdb1e499cb4b32d7137d7fce72352184597e8
David Madden <d.d.madden@bham.ac.uk>
2022-07-26 20:52:39 +0100

    fix analytics

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,16 @@
 import adapter from "@sveltejs/adapter-vercel";
+import preprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
+  preprocess: preprocess({
+    replace: [
+      [
+        "import.meta.env.VERCEL_ANALYTICS_ID",
+        JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
+      ],
+    ],
+  }),
   kit: {
     adapter: adapter({ edge: true }),
   },
