4778da45756f353cc710f55eee5f16616303b073
David Madden <d.d.madden@bham.ac.uk>
2022-07-27 21:34:46 +0100

    added colour

--- src/lib/Element.svelte
+++ src/lib/Element.svelte
@@ -1,8 +1,30 @@
 <script>
   export let element;
+
+  function blockColor() {
+    // f-block
+    if ([9, 10].includes(element.ypos)) {
+      return "#9bff99"
+    }
+
+    // s-block
+    if (([1, 2].includes(element.xpos)) || (element.xpos === 18 && element.ypos === 1)) {
+      return "#ff9999"
+    }
+
+    // d-block
+    if ( (element.xpos > 2 && element.xpos < 13) && (element.ypos > 2 && element.ypos < 13)) {
+      return "#99ccff";
+    }
+
+    // p-block
+    if ((element.xpos > 12) && (element.ypos > 1 && element.ypos < 8)) {
+      return "#fdff8c";
+    }
+  }
 </script>
 
-<div class="element">
+<div class="element" style="background-color: {blockColor()}">
   <span class="symbol sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">{element.symbol}</span>
 </div>
 
